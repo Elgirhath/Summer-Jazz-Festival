9c7f34bfead19777aa49d0e788288fac64be5e62
KonradPolarczyk <konradpolarczyk98@gmail.com>
2020-06-29 23:13:04 +0200

    Search on enter and new ticket

--- app/src/main/java/com/example/festivalapp/schedule/ProgramFragment.java
+++ app/src/main/java/com/example/festivalapp/schedule/ProgramFragment.java
@@ -13,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Vibrator;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -71,6 +72,7 @@ public class ProgramFragment extends Fragment {
 
                 Vibrator vibe = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
                 vibe.vibrate(30);
+                recyclerView.scrollToPosition(0);
             }
         });
 
@@ -83,6 +85,7 @@ public class ProgramFragment extends Fragment {
             public void onClick(View view) {
                 searchInput.setText("");
                 adapter.applyQuery("");
+                recyclerView.scrollToPosition(0);
             }
         });
 
@@ -106,6 +109,15 @@ public class ProgramFragment extends Fragment {
             public void afterTextChanged(Editable editable) {
 
             }
+        });
+
+        searchInput.setOnKeyListener((v, keyCode, event) -> {
+            if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                    (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                searchButton.callOnClick();
+                return true;
+            }
+            return false;
         });
 
         final RelativeLayout searchBar = getView().findViewById(R.id.searchBar);
